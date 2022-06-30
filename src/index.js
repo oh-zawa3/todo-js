a707511809271980c34f80b58030c984af131732
oh-zawa3 <97651385+oh-zawa3@users.noreply.github.com>
2022-06-30 22:24:26 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const onClickAdd = () => {
+  //テキストボックスの値を取得し。初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //liタグ生成
+  const li = document.createElement("li");
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //pタグ生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  //li,divの子要素に各要素を設定
+  li.appendChild(div);
+  div.appendChild(p);
+
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
